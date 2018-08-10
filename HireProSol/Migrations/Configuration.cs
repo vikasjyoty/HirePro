1046d0ebe63b90b37d884aad7a528271f7c9a992
vikasjyoty <vikasj1002@gmail.com>
2018-08-09 19:07:35 -0500

    Added MySql Database

---
+++ HireProSol/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+namespace EdisonWebMVC.Migrations
+{
+    using HireProSol.Models;
+    using System.Data.Entity.Migrations;
+    /// <summary>
+    /// https://msdn.microsoft.com/en-us/data/jj591621.aspx
+    /// Above is an important link for commnads of migration process
+    /// </summary>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("MySql.Data.MySqlClient", new MySql.Data.Entity.MySqlMigrationSqlGenerator());
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+
+        }
+    }
+}
---
+++ HireProSol/Models/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace HireProSol.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public ApplicationDbContext()
+            : base("MyContext")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}
