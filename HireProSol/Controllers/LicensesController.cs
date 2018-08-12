4e7f6b712456df55fd393f5e9c9d3d6380f2a53a
vikasjyoty <vikasj1002@gmail.com>
2018-08-12 17:09:30 -0500

    License added

---
+++ HireProSol/Controllers/LicensesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HireProSol.Models;
+
+namespace HireProSol.Controllers
+{
+    public class LicensesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Licenses
+        public ActionResult Index()
+        {
+            var licenses = db.Licenses.Include(l => l.Type);
+            return View(licenses.ToList());
+        }
+
+        // GET: Licenses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            License license = db.Licenses.Find(id);
+            if (license == null)
+            {
+                return HttpNotFound();
+            }
+            return View(license);
+        }
+
+        // GET: Licenses/Create
+        public ActionResult Create()
+        {
+            ViewBag.LicenseType_Id = new SelectList(db.LicenseTypes, "Id", "LicenseTypeDesc");
+            License newLicense = new License();
+            return View(newLicense);
+        }
+
+        // POST: Licenses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Key,CreatedOn,IsBlocked,BlockedOn,LicenseType_Id")] License license)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Licenses.Add(license);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LicenseType_Id = new SelectList(db.LicenseTypes, "Id", "LicenseTypeDesc", license.LicenseType_Id);
+            return View(license);
+        }
+
+        // GET: Licenses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            License license = db.Licenses.Find(id);
+            if (license == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LicenseType_Id = new SelectList(db.LicenseTypes, "Id", "LicenseTypeDesc", license.LicenseType_Id);
+            return View(license);
+        }
+
+        // POST: Licenses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Key,CreatedOn,IsBlocked,BlockedOn,LicenseType_Id")] License license)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(license).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.LicenseType_Id = new SelectList(db.LicenseTypes, "Id", "LicenseTypeDesc", license.LicenseType_Id);
+            return View(license);
+        }
+
+        // GET: Licenses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            License license = db.Licenses.Find(id);
+            if (license == null)
+            {
+                return HttpNotFound();
+            }
+            return View(license);
+        }
+
+        // POST: Licenses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            License license = db.Licenses.Find(id);
+            db.Licenses.Remove(license);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
